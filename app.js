45a320f15b8f83d53a5cd42304dfd15c66460d41
Josue Diaz <jdiaz66@ledora-lan.lan>
2019-02-18 14:13:16 +0000

    wrapped file being sent into quotes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express"),
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send(index.html);
+  res.send("index.html");
 });
 
 app.listen(port, () => {
